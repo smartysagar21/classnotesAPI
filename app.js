e51218e22b32bd8f5c80f1b5065d465a63f0f183
smartysagar21 <smartysagar21@gmail.com>
2023-08-19 15:35:32 +0530

    fixed engine

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ config({
 //Using Middlewares
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
